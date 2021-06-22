d61d0d6697135015dc664a7d15189023e0c64d48
Tilanka_Mihingu <tilankamihingu2018@gmail.com>
2021-06-22 20:54:35 +0530

    product page

--- src/Pages/Products/Products.jsx
+++ src/Pages/Products/Products.jsx
@@ -1,9 +1,30 @@
-import React from 'react'
+import React, { useContext } from 'react';
+import { DataContext } from '../../components/Context';
+import './Products.css';
+import Cim from '../../images/green.jpg';
 
 const Products = () => {
+    const value = useContext(DataContext)
+    const [products] = value.products
     return (
-        <div>
-            PRduct page
+        <div className="product-full">
+        <div className="product-container">
+            <div className="product-row">
+                {
+                    products.map(product => (
+                        <div className="product-card">
+                            <img src={product.src} alt="" />
+                            <div className="card-details">
+                                <h2>{product.title}</h2>
+                                <p>{product.description}</p>
+                                <h3>{product.price}</h3>
+                                <button className="card-btn">View</button>
+                            </div>
+                        </div>
+                    ))
+                }
+            </div>
+        </div>
         </div>
     )
 }
